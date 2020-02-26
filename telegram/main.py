db9e57385246125fcf288854d4ce29f26b856696
lrmendes98 <lrmmendes98@gmail.com>
2020-02-26 00:19:41 +0000

    Added alarm clock at 6h50, hope it works :)

--- telegram/main.py
+++ telegram/main.py
@@ -4,8 +4,18 @@ from system import *
 import subprocess
 
 # Start telegram bot
-updater.start_polling()
+# updater.start_polling()
 
 # thread = Thread(target = fifoReader, args = ())
 # thread.start()
 
+import datetime
+now = datetime.datetime.now()
+while (1):
+    time.sleep(60)
+    now = datetime.datetime.now()
+    if now.hour == 6 and now.minute == 50:
+        bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on'")
+        bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 2=on && python3 ~/rush/energienie.py 2=on'")
+        time.sleep(5)
+        bash_call("ssh pi@192.168.1.106 'omxplayer ~/rush/wakeup_songs/03.\ Lakeside\ Park.mp3'")
