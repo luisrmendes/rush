8f8dd3c33aeeafb4a611f422b5360824cb4a5f5b
luis <lrmmendes98@gmail.com>
2020-08-13 15:18:13 +0100

    Finished django tutorial, added energienie.py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 
 from . import views
 
-#app_name= 'polls'
+app_name= 'polls'
 
 urlpatterns = [
     path('', views.index, name='index'),
-
     #127.0.0.1/polls 
     path('<int:question_id>/', views.detail, name='detail'), 
 
