8a0bf3d088a59e2bf6b2d05b2f9f693b42c1d1b6
luis <lrmmendes98@gmail.com>
2020-08-13 15:18:13 +0100

    Finished django tutorial, added energienie.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,29 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date') [:5]
-    output = ", ".join(q.question_text for q  in latest_questions)
-    return HttpResponse(output)
+    context = {'latest_questions':latest_questions}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("This is the detail view of the question: %s" % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    return HttpResponse("These are the results of the question: %s" % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    return HttpResponse("Vote on question: %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk = request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': "Please select a choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=[question.id]))
