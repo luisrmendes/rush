be90d79c3963b653029f51671f44970595ac82fd
lrmmendes98 <lrmmendes98@gmail.com>
2020-08-13 12:42:24 +0100

    Moving to Django

---
+++ mysite/polls/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from .models import Question
+
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date') [:5]
+    output = ", ".join(q.question_text for q  in latest_questions)
+    return HttpResponse(output)
+
+def detail(request, question_id):
+    return HttpResponse("This is the detail view of the question: %s" % question_id)
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question: %s" % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: %s" % question_id)
