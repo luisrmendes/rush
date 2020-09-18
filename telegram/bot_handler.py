84ca72b57834dec4730d95876e6fbea5aafeae11
lrmmendes98 <lrmmendes98@gmail.com>
2020-09-18 21:54:27 +0100

    Renaming some commands

--- telegram/bot_handler.py
+++ telegram/bot_handler.py
@@ -14,46 +14,46 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 
-# /sock2_on command
-def sock2_on(update, context):
-    result = bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 2=on && python3 ~/rush/energienie.py 2=on'")
-    updater.bot.send_message(chat_id=update.effective_chat.id, text=result)
-    
-sock2_on_handler = CommandHandler('sock2_on', sock2_on)
-dispatcher.add_handler(sock2_on_handler)
-
-# /sock1_on command
-def sock1_on(update, context):
+# /speakersOn command
+def speakers_on(update, context):
     result = bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on'")
     updater.bot.send_message(chat_id=update.effective_chat.id, text=result)
     
-sock1_on_handler = CommandHandler('sock1_on', sock1_on)
-dispatcher.add_handler(sock1_on_handler)
+speakers_on_handler = CommandHandler('speakers_on', speakers_on)
+dispatcher.add_handler(speakers_on_handler)
 
-# /sock2_off command
-def sock2_off(update, context):
-    result = bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 2=off && python3 ~/rush/energienie.py 2=off'")
-    updater.bot.send_message(chat_id=update.effective_chat.id, text=result)
-    
-sock2_off_handler = CommandHandler('sock2_off', sock2_off)
-dispatcher.add_handler(sock2_off_handler)
-
-# /sock1_off command
-def sock1_off(update, context):
+# /speakers_off command
+def speakers_off(update, context):
     result = bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 1=off && python3 ~/rush/energienie.py 1=off'")
     updater.bot.send_message(chat_id=update.effective_chat.id, text=result)
     
-sock1_off_handler = CommandHandler('sock1_off', sock1_off)
-dispatcher.add_handler(sock1_off_handler)
+speakers_off_handler = CommandHandler('speakers_off', speakers_off)
+dispatcher.add_handler(speakers_off_handler)
+
+# /lights_off command
+def lights_off(update, context):
+    result = bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 2=off && python3 ~/rush/energienie.py 2=off'")
+    updater.bot.send_message(chat_id=update.effective_chat.id, text=result)
+    
+lights_off_handler = CommandHandler('lights_off', lights_off)
+dispatcher.add_handler(lights_off_handler)
+
+# /lights_on command
+def lights_on(update, context):
+    result = bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 2=on && python3 ~/rush/energienie.py 2=on'")
+    updater.bot.send_message(chat_id=update.effective_chat.id, text=result)
+    
+lights_on_handler = CommandHandler('lights_on', lights_on)
+dispatcher.add_handler(lights_on_handler)
 
 # /rpi_play command
 # youtube-dl --extract-audio --audio-format mp3 <link> https:/&& omxplayer Rush\ -\ Clockwork\ Angels\ Tour\ -\ The\ Garden-EsBNzf5JlZA.mkv
-def rpi_play(update, context):
+def youtube_play(update, context):
     result = bash_call("ssh pi@192.168.1.106 'omxplayer ~/rush/wakeup_songs/03.\ Lakeside\ Park.mp3'")
     updater.bot.send_message(chat_id=update.effective_chat.id, text=result)
     
-rpi_play_handler = CommandHandler('rpi_play', rpi_play)
-dispatcher.add_handler(rpi_play_handler)
+youtube_play_handler = CommandHandler('youtube_play', youtube_play)
+dispatcher.add_handler(youtube_play_handler)
 
 # /shutdown command
 def shutdown_desktop(update, context):
