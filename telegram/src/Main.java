c762de2a7c6861d64e9dd4d49630d157ea543b45
lrmmendes98 <lrmmendes98@gmail.com>
2020-09-18 20:32:43 +0100

    Cannot find main class

--- telegram/src/Main.java
+++ telegram/src/Main.java
@@ -1,6 +1,7 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 public class Main {
     public static void main(String[] args) {
         // Initialize Api Context
