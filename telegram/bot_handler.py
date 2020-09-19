800a5b27771fccb9042c0cab10216a686478b2b8
lrmendes98 <lrmmendes98@gmail.com>
2020-09-19 14:25:18 +0100

    Renamed telegram.old to telegram_java, changed youtube play command to detach pid

--- telegram/bot_handler.py
+++ telegram/bot_handler.py
@@ -52,7 +52,7 @@ def youtube_play(update, context):
     bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on'")
     command = "ssh pi@192.168.1.106 'rm -f play.mp3 && youtube-dl --extract-audio --audio-format mp3 -o 'play.mp3' "
     command += context.args[0]
-    command += " && omxplayer play.mp3'"
+    command += " && omxplayer play.mp3 &'"
     result = bash_call(command)
 
     print(command)
