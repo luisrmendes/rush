5e3517c78c96730690a4b76c70047d19c278c85a
lrmmendes98 <lrmmendes98@gmail.com>
2020-09-19 11:53:52 +0100

    changed to sv_brightness, deleted non-sensical loop

--- telegram/main.py
+++ telegram/main.py
@@ -11,9 +11,7 @@ updater.start_polling()
 
 import datetime
 now = datetime.datetime.now()
-while (1):
-    time.sleep(60)
-    now = datetime.datetime.now()
+
     # if now.hour == 6 and now.minute == 50:
     #     bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on'")
     #     bash_call("ssh pi@192.168.1.106 'python3 ~/rush/energienie.py 2=on && python3 ~/rush/energienie.py 2=on'")
