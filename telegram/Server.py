c247be3c1a9334b50deeb78dd6361b78fb908cff
lrmmendes98 <lrmmendes98@gmail.com>
2020-09-23 00:13:23 +0100

    Implemented Threaded bash calls

--- telegram/Server.py
+++ telegram/Server.py
@@ -1,15 +1,15 @@
-from system import bash_call
+from aux import *
 
 class Server:
     
     def getIpv4(self):
-        result = bash_call("dig @resolver1.opendns.com A myip.opendns.com +short -4")
+        result = bashCall("dig @resolver1.opendns.com A myip.opendns.com +short -4")
         return result
     
     def getIpv6(self):
-        result = bash_call("dig @resolver1.opendns.com AAAA myip.opendns.com +short -6")
+        result = bashCall("dig @resolver1.opendns.com AAAA myip.opendns.com +short -6")
         return result
     
     def serverBrightness(self, context):
-        result = bash_call("sudo sh /home/luis/github/dotFiles/brightness.sh " + context.args[0])
+        result = bashCall("sudo sh /home/luis/github/dotFiles/brightness.sh " + context.args[0])
         return result
