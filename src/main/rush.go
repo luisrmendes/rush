15dbc0abb86c9b2dea7831230098d1e6cc3d385e
luisrmendes <lrmmendes98@gmail.com>
2022-10-20 23:31:49 +0100

    reduce delay

--- src/main/rush.go
+++ src/main/rush.go
@@ -69,7 +69,7 @@ func readSensorData(wg *sync.WaitGroup, sData *sensorData) {
 
 func handleSensorData(wg *sync.WaitGroup, sData *sensorData) {
 	for {
-		time.Sleep(50000000)
+		time.Sleep(5000000)
 		if sData.brightness == 0 {
 			continue
 		}
