1aeb3135cf3dfa50be46765226becc8697c96b3b
luisrmendes <lrmmendes98@gmail.com>
2022-10-20 18:45:21 +0100

    go tcp client now handles tcp server shutdown

--- src/sensorDataHandler/sensorDataHandler.go
+++ src/sensorDataHandler/sensorDataHandler.go
@@ -10,6 +10,7 @@ import (
 )
 
 func ReadSensorData(wg *sync.WaitGroup) {
+	// Fetch environment variables
 	esp8266_address_port := os.Getenv("ESP8266_ADDRESS_PORT")
 	if esp8266_address_port == "" {
 		log.Printf("ESP8266_ADDRESS_PORT not found in env vars, checking .env")
@@ -21,20 +22,26 @@ func ReadSensorData(wg *sync.WaitGroup) {
 	esp8266_address_port = os.Getenv("ESP8266_ADDRESS_PORT")
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Some error occured. Err: %s", err)
+		log.Fatalf("Some error occured. Err: %s", err)
 	}
 
-	connection, err := net.Dial("tcp", esp8266_address_port)
-	if err != nil {
-		panic(err)
-	}
 	for {
-		buffer := make([]byte, 1024)
-		mLen, err := connection.Read(buffer)
+		connection, err := net.Dial("tcp", esp8266_address_port)
 		if err != nil {
-			fmt.Println("Error reading:", err.Error())
+			panic(err)
+		} else {
+			log.Println("Connected to " + esp8266_address_port)
 		}
-		fmt.Println("Received: ", string(buffer[:mLen]))
+		for {
+			buffer := make([]byte, 1024)
+			mLen, err := connection.Read(buffer)
+			if err != nil {
+				log.Println("Error reading:", err.Error())
+				log.Println("Disconnecting...")
+				break
+			}
+			fmt.Println("Received: ", string(buffer[:mLen]))
+		}
 	}
 	defer wg.Done()
 }
