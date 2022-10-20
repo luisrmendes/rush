654e59595fed3b9ba4e8df736e714d7ddd32eee9
luisrmendes <lrmmendes98@gmail.com>
2022-10-20 23:12:26 +0100

    remove more unecessary panics

--- src/telegramBot/telegramBot.go
+++ src/telegramBot/telegramBot.go
@@ -63,7 +63,7 @@ func PollUpdates(wg *sync.WaitGroup) {
 	telegram_api_key = os.Getenv("TELEGRAM_API_KEY")
 	bot, err := tgbotapi.NewBotAPI(telegram_api_key)
 	if err != nil {
-		log.Panic(err)
+		log.Println(err)
 	}
 
 	// bot.Debug = true
