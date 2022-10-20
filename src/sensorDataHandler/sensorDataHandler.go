f6cc5f150e421d283f56d4583fc9c1196465d577
luisrmendes <lrmmendes98@gmail.com>
2022-10-20 01:08:53 +0100

    fixing env vars stuff

--- src/sensorDataHandler/sensorDataHandler.go
+++ src/sensorDataHandler/sensorDataHandler.go
@@ -3,19 +3,26 @@ package sensorDataHandler
 import (
 	"fmt"
 	"github.com/joho/godotenv"
+	"log"
 	"net"
 	"os"
 	"sync"
 )
 
-
 func ReadSensorData(wg *sync.WaitGroup) {
+	esp8266_address_port := os.Getenv("ESP8266_ADDRESS_PORT")
+	if esp8266_address_port == "" {
+		log.Printf("ESP8266_ADDRESS_PORT not found in env vars, checking .env")
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatalf("Some error occured. Err: %s", err)
+		}
+	}
+	esp8266_address_port = os.Getenv("ESP8266_ADDRESS_PORT")
 	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Some error occured. Err: %s", err)
 	}
-
-	esp8266_address_port := os.Getenv("ESP8266_ADDRESS_PORT")
 
 	connection, err := net.Dial("tcp", esp8266_address_port)
 	if err != nil {
