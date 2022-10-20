981490b73916fc055564a3d65807de796f5d78c7
luisrmendes <lrmmendes98@gmail.com>
2022-10-20 23:51:11 +0100

    add difference checker for adjusting brightness

--- src/utils/utils.go
+++ src/utils/utils.go
@@ -1,35 +1,39 @@
 package utils
 
 import (
+	"log"
 	"os/exec"
 	"strconv"
-	"log"
 )
 
 func setDesktopBrightness(brightness int) {
 	brightStr := strconv.Itoa(brightness)
-	execute("ssh", "desktop", "sudo ddcutil --bus 6 setvcp 10 " + brightStr)
+	execute("ssh", "desktop", "sudo ddcutil --bus 6 setvcp 10 "+brightStr)
 }
 
 // Maybe some linear regression stuff would be cool
 func ControlDesktopBrightness(brightness int) {
-	switch {
-	case brightness >= 800:
-		setDesktopBrightness(100)
-	case brightness < 800 && brightness >= 600:
-		setDesktopBrightness(80)
-	case brightness < 600 && brightness >= 500:
-		setDesktopBrightness(60)
-	case brightness < 500 && brightness >= 400:
-		setDesktopBrightness(50)
-	case brightness < 400 && brightness >= 300:
-		setDesktopBrightness(30)
-	case brightness < 300 && brightness >= 200:
-		setDesktopBrightness(20)
-	case brightness < 200:
-		setDesktopBrightness(0)
+	previousValue := 0
+	if absDiff(previousValue, brightness) > 100 {
+		log.Println("Sending brightness command")
+		switch {
+		case brightness >= 800:
+			setDesktopBrightness(100)
+		case brightness < 800 && brightness >= 600:
+			setDesktopBrightness(80)
+		case brightness < 600 && brightness >= 500:
+			setDesktopBrightness(60)
+		case brightness < 500 && brightness >= 400:
+			setDesktopBrightness(50)
+		case brightness < 400 && brightness >= 300:
+			setDesktopBrightness(30)
+		case brightness < 300 && brightness >= 200:
+			setDesktopBrightness(20)
+		case brightness < 200:
+			setDesktopBrightness(0)
+		}
 	}
-	
+	previousValue = brightness
 }
 
 func execute(name string, args ...string) string {
@@ -40,4 +44,11 @@ func execute(name string, args ...string) string {
 	output := string(out[:])
 
 	return output
+}
+
+func absDiff(a, b int) int {
+	if a < b {
+		return b - a
+	}
+	return a - b
 }
