2082b001c1a3f1f272d4a0c7b826f33912aeedd8
luisrmendes <lrmmendes98@gmail.com>
2022-10-21 20:36:39 +0100

    fix rpi socket commands

--- src/devicesController/rpiController.go
+++ src/devicesController/rpiController.go
@@ -5,13 +5,15 @@ import (
 )
 
 func RpiTurnOnSockets() string {
-	ret := utils.Execute("ssh", "pi@192.168.1.107", "python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on" + 
+	ret := utils.Execute("ssh", "pi@192.168.1.107", 
+		"python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on && python3 ~/rush/energienie.py 1=on && "+
 		"python3 ~/rush/energienie.py 2=on && python3 ~/rush/energienie.py 2=on && python3 ~/rush/energienie.py 2=on")
 	return ret
 }
 
 func RpiTurnOffSockets() string {
-	ret := utils.Execute("ssh", "pi@192.168.1.107", "python3 ~/rush/energienie.py 1=off && python3 ~/rush/energienie.py 1=off && python3 ~/rush/energienie.py 1=off" + 
+	ret := utils.Execute("ssh", "pi@192.168.1.107", 
+		"python3 ~/rush/energienie.py 1=off && python3 ~/rush/energienie.py 1=off && python3 ~/rush/energienie.py 1=off && "+
 		"python3 ~/rush/energienie.py 2=off && python3 ~/rush/energienie.py 2=off && python3 ~/rush/energienie.py 2=off")
 	return ret
 }
