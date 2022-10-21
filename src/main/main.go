9823261c546fa89a79d4bfde36862ebbcc9fe685
luisrmendes <lrmmendes98@gmail.com>
2022-10-21 13:06:25 +0100

    correting gitignore

---
+++ src/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"example.com/telegramBot"
+	"example.com/sensorDataHandler"
+	"sync"
+)
+
+func main() {
+	var wg = &sync.WaitGroup{}
+	var sData sensorDataHandler.SensorData
+	wg.Add(3)
+
+	go telegramBot.PollUpdates(wg)
+
+	go sensorDataHandler.ReadSensorData(wg, &sData)
+
+	go sensorDataHandler.HandleSensorData(wg, &sData)
+
+	wg.Wait()
+}
