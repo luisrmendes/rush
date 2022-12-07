b6df4ae302fcef8031f9c0afcf20f8676af75d3c
luisrmmendes <luis.rm.mendes@ctw.bmwgroup.com>
2022-12-07 16:43:24 +0000

    ...

--- src/devicesController/desktopController.go
+++ src/devicesController/desktopController.go
@@ -55,7 +55,7 @@ func ControlDesktopBrightness(sensorBrightness int) {
 	// }
 
 	var coef float64 = 0.14285714285714285
-	if sensorBrightness >= 150 {
+	if sensorBrightness >= 90 {
 		setMonitorBrightness = int(math.Round(float64(sensorBrightness) * coef))
 	} else {
 		setMonitorBrightness = 0
