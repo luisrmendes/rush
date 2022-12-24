bb13b8da634ba6f4344d285566481fc92b4283f4
luisrmendes <lrmmendes98@gmail.com>
2022-12-24 01:39:23 +0000

    chage to 1 ping

--- src/devicesController/desktopController.go
+++ src/devicesController/desktopController.go
@@ -19,7 +19,7 @@ var brightControlPQ = utils.NewPriorityQueue()
 // Checks if desktop is online and what OS is running
 func GetDesktopStatus() string {
 	work_laptop_address := os.Getenv("WORK_LAPTOP_ADDRESS")
-	output := utils.Execute("ping", "-c", "2", work_laptop_address)
+	output := utils.Execute("ping", "-c", "1", work_laptop_address)
 	fmt.Printf("ola, %s", output)
 	return output
 }
