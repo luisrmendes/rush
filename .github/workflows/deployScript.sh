9efee1aeb7b8accf15707092bd4a1dfc2cc04617
Luis Mendes <32670238+luisrmendes@users.noreply.github.com>
2024-02-13 09:33:06 +0000

    Update deployScript.sh

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-if tmux list-windows | grep -q 'ambrosioBot' 
+if tmux list-windows | grep -q 'rushBot' 
 then
     echo "Session exists"
 else
     echo "Session does not exist, creating sesion";
-    tmux new-session -s ambrosioBot;
+    tmux new-session -s rushBot;
 fi
 
-tmux send-keys -t ambrosioBot "^C" Enter;
+tmux send-keys -t rushBot "^C" Enter;
 cd ./src/main;
 mv main /home/lrm/sideProjs/ambrosioBot;
-tmux send-keys -t ambrosioBot "cd /home/lrm/sideProjs/ambrosioBot" Enter;
-tmux send-keys -t ambrosioBot "TELEGRAM_API_KEY=";
-tmux send-keys -t ambrosioBot $1;
-tmux send-keys -t ambrosioBot " ESP8266_ADDRESS_PORT=";
-tmux send-keys -t ambrosioBot $2;
-tmux send-keys -t ambrosioBot " SYSTEM_1_ADDRESS=";
-tmux send-keys -t ambrosioBot $3;
-tmux send-keys -t ambrosioBot " SYSTEM_2_ADDRESS=";
-tmux send-keys -t ambrosioBot $4;
-tmux send-keys -t ambrosioBot " ./main" Enter;
+tmux send-keys -t rushBot "cd /home/lrm/sideProjs/ambrosioBot" Enter;
+tmux send-keys -t rushBot "TELEGRAM_API_KEY=";
+tmux send-keys -t rushBot $1;
+tmux send-keys -t rushBot " ESP8266_ADDRESS_PORT=";
+tmux send-keys -t rushBot $2;
+tmux send-keys -t rushBot " SYSTEM_1_ADDRESS=";
+tmux send-keys -t rushBot $3;
+tmux send-keys -t rushBot " SYSTEM_2_ADDRESS=";
+tmux send-keys -t rushBot $4;
+tmux send-keys -t rushBot " ./main" Enter;
