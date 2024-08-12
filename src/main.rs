48280280ea9b14ee9ecde495b7233df0cbfe708f
luisrmendes <lrmmendes98@gmail.com>
2024-08-12 22:11:13 +0100

    add commands and telegram module

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,115 @@
-use std::future::Future;
-
-use tokio::io::{self, AsyncReadExt};
+use telegram::get_telegram_task;
+use tokio::io::{self};
 use tokio::net::TcpStream;
-use tokio::task;
 
-#[derive(Debug)]
-pub struct OfficeEnv {
-    brightness: u16,
-    temperature: u16,
-    humidity: u16,
+pub enum Operation {
+    GetIpv4,
+    GetIpv6,
+    WakeupDesktop,
 }
 
-async fn get_telegram_task()  {
-    use teloxide::prelude::*;
+pub mod commands {
+    use crate::Operation;
 
-    pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
-
-    let bot = Bot::from_env();
-
-    return teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    }).await;
+    pub fn execute_commands(op: Operation) {
+        match op {
+            Operation::GetIpv4 => {}
+            Operation::GetIpv6 => {}
+            Operation::WakeupDesktop => {}
+            _ => {}
+        }
+    }
 }
 
-/// Fetches Office Environment Data from the ESP8266 TCP Server
-fn get_office_env_task(mut stream: TcpStream) -> tokio::task::JoinHandle<Result<OfficeEnv, ()>> {
-    return task::spawn(async move {
-        let mut buffer = [0; 512];
+pub mod telegram {
+    use crate::Operation;
 
-        let recv_length: usize = match stream.read(&mut buffer).await {
-            Ok(0) => {
-                println!("Connection closed by the server");
-                return Err(());
-            }
-            Ok(n) => {
-                println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
-                n
-            }
-            Err(e) => {
-                eprintln!("Failed to read from socket; error = {:?}", e);
-                return Err(());
-            }
+    fn parse_commands(text: Option<&str>) -> Option<Operation> {
+        let parsed_msg: &str = match text {
+            Some(msg) => msg,
+            None => "",
         };
+        match parsed_msg {
+            "/ipv4" => Some(Operation::GetIpv4),
+            "/ipv6" => Some(Operation::GetIpv6),
+            "/desktop_wakeup" => Some(Operation::WakeupDesktop),
+            _ => None,
+        }
+    }
 
-        let result: Result<Vec<u16>, _> = String::from_utf8_lossy(&buffer[..recv_length])
-            .split_whitespace()
-            .map(|s| s.parse::<u16>())
-            .collect();
+    pub async fn get_telegram_task() {
+        use teloxide::prelude::*;
 
-        let env_data: OfficeEnv = match result {
-            Ok(result) => OfficeEnv {
-                brightness: result[1],
-                temperature: result[2],
-                humidity: result[3],
-            },
-            Err(e) => {
-                eprintln!("Failed parse info; error = {:?}", e);
-                return Err(());
-            }
-        };
+        pretty_env_logger::init();
+        log::info!("Starting throw dice bot...");
 
-        return Ok(env_data);
-    });
+        let bot = Bot::from_env();
+
+        return teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+            println!("{:?}", msg.text());
+
+            let command = parse_commands(msg.text());
+            bot.send_dice(msg.chat.id).await?;
+            Ok(())
+        })
+        .await;
+    }
+}
+
+pub mod office_env {
+    use tokio::io::AsyncReadExt;
+    use tokio::net::TcpStream;
+    use tokio::task;
+
+    #[derive(Debug)]
+    pub struct OfficeEnv {
+        brightness: u16,
+        temperature: u16,
+        humidity: u16,
+    }
+
+    /// Fetches Office Environment Data from the ESP8266 TCP Server
+    pub fn get_office_env_task(
+        mut stream: TcpStream,
+    ) -> tokio::task::JoinHandle<Result<OfficeEnv, ()>> {
+        return task::spawn(async move {
+            let mut buffer = [0; 512];
+
+            let recv_length: usize = match stream.read(&mut buffer).await {
+                Ok(0) => {
+                    println!("Connection closed by the server");
+                    return Err(());
+                }
+                Ok(n) => {
+                    println!("Received: {}", String::from_utf8_lossy(&buffer[..n]));
+                    n
+                }
+                Err(e) => {
+                    eprintln!("Failed to read from socket; error = {:?}", e);
+                    return Err(());
+                }
+            };
+
+            let env_data: Result<Vec<u16>, _> = String::from_utf8_lossy(&buffer[..recv_length])
+                .split_whitespace()
+                .map(|s| s.parse::<u16>())
+                .collect();
+
+            let env_data: OfficeEnv = match env_data {
+                Ok(env_data) => OfficeEnv {
+                    brightness: env_data[1],
+                    temperature: env_data[2],
+                    humidity: env_data[3],
+                },
+                Err(e) => {
+                    eprintln!("Failed parse info; error = {:?}", e);
+                    return Err(());
+                }
+            };
+
+            return Ok(env_data);
+        });
+    }
 }
 
 #[tokio::main]
@@ -71,7 +117,7 @@ async fn main() -> io::Result<()> {
     let stream = TcpStream::connect("192.168.1.69:4080").await?;
     println!("Connected to the server!");
 
-    match get_office_env_task(stream).await {
+    match office_env::get_office_env_task(stream).await {
         Ok(Ok(office_env)) => {
             println!("Final buffer received: {:?}", office_env);
         }
