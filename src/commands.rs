d1219b89361d9f07f6747f4ea343ef41fcc368e5
luisrmendes <lrmmendes98@gmail.com>
2024-08-27 20:21:12 +0100

    add do not tui if rustlog is trace, one more device as my phone

--- src/commands.rs
+++ src/commands.rs
@@ -48,7 +48,7 @@ pub async fn get_am_i_home(global_state: Arc<Mutex<GlobalState>>) {
 
         match send_command("nmap -T5 -sn 192.168.1.0/24", None).await {
             Ok(out) => {
-                if out.contains("unknownbc7fa4343f2d") {
+                if out.contains("unknownbc7fa4343f2d") || out.contains("RedmiNote9S-Lu*sRed") {
                     global_state.lock().await.am_i_home = true;
                     AM_I_NOT_AT_HOME_COUNTER.store(0, Ordering::SeqCst);
                 } else if am_i_at_home {
