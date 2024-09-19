1f142d9e2d456a527fb6133cbb414ba57625f85d
luisrmendes <lrmmendes98@gmail.com>
2024-09-19 17:11:03 +0100

    add multiple monitor support on brightness control

--- src/snowdog_ctrl_fsm.rs
+++ src/snowdog_ctrl_fsm.rs
@@ -6,6 +6,8 @@ use log::debug;
 use log::error;
 use openssh::KnownHosts;
 use openssh::Session;
+use ping_rs::send_ping_async;
+use regex::Regex;
 use std::sync::atomic::AtomicU32;
 use std::sync::atomic::Ordering;
 use std::sync::Arc;
@@ -26,6 +28,22 @@ pub struct Fsm {
 }
 
 impl Fsm {
+    async fn get_i2c_monitor_numbers(session: &Session) -> Result<Vec<String>, String> {
+        // get available monitors
+        let monitor_list_out = send_command("ddcutil detect", Some(session)).await?;
+
+        // parse monitor bus'
+        let re = Regex::new(r"/dev/i2c-(\d+)").unwrap();
+
+        // Find all matches and capture the last digit part
+        let matches: Vec<String> = re
+            .captures_iter(&monitor_list_out) // Iterate over all captures
+            .map(|cap| cap[1].to_string()) // Capture group 1 contains the digit after `/dev/i2c-`
+            .collect();
+
+        Ok(matches)
+    }
+
     async fn connecting(&mut self) {
         debug!("Connecting");
         let session_access: &str = &(self.pc.user.clone() + "@" + &self.pc.ip);
@@ -51,21 +69,33 @@ impl Fsm {
             return;
         };
 
+        let monitor_i2c_numbers = match Self::get_i2c_monitor_numbers(session).await {
+            Ok(out) => out,
+            Err(e) => {
+                error!("{e}");
+                sleep(Duration::from_millis(500)).await;
+                return;
+            }
+        };
+
         let env_brightness = self.global_state.lock().await.office_env.brightness;
         let mon_brightness = calculate_ddc_mon_brightness(env_brightness);
 
         // only send command if calculated brightness is different than the previously sent one
         if MON_BRIGHTNESS.load(Ordering::Relaxed) == mon_brightness {
-            debug!("Same brightness calculated. Static brightness: {MON_BRIGHTNESS:?}, brightness: {mon_brightness}");
             sleep(Duration::from_millis(500)).await;
             return;
         }
 
         MON_BRIGHTNESS.store(mon_brightness, Ordering::SeqCst);
 
-        let command: String = format!("ddcutil --bus 6 setvcp 10 {mon_brightness}");
+        // build command string
+        let mut command_builder = String::new();
+        for i2c_num in monitor_i2c_numbers {
+            command_builder += &format!("ddcutil --bus {i2c_num} setvcp 10 {mon_brightness} & ");
+        }
 
-        match send_command(&command, Some(session)).await {
+        match send_command(&command_builder, Some(session)).await {
             Ok(out) => {
                 debug!("{out}");
             }
