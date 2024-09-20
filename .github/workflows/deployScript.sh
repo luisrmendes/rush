51257fab4297c5a5f64d076797a33c6023b389c6
luisrmendes <lrmmendes98@gmail.com>
2024-09-20 17:59:29 +0100

    update actions

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -13,9 +13,11 @@ fi
 
 tmux send-keys -t $tmuxSessionName "^C" Enter
 tmux send-keys -t $tmuxSessionName "^C" Enter
+tmux send-keys -t $tmuxSessionName "sleep 2 && reset && sleep 1" Enter
+
 
 tmuxSeshCommand=""
-tmuxSeshCommand="${tmuxSeshCommand} sleep 2 && reset && sleep 1 && TELOXIDE_TOKEN=$1 ESP8266_ADDRESS_PORT=$2 SYRINX_VARS=$3 SNOWDOG_VARS=$4 THINKPADX1_VARS=$5 RPI5_VARS=$6 RPI3_VARS=$7"
+tmuxSeshCommand="${tmuxSeshCommand} TELOXIDE_TOKEN=$1 ESP8266_ADDRESS_PORT=$2 SYRINX_VARS=$3 SNOWDOG_VARS=$4 THINKPADX1_VARS=$5 RPI5_VARS=$6 RPI3_VARS=$7"
 tmuxSeshCommand="${tmuxSeshCommand} RUST_LOG=warn target/release/rush"
 
 tmux send-keys -t $tmuxSessionName "${tmuxSeshCommand}" Enter
