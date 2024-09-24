3f1cf7e35887460bb8de5253e8e2819c25554014
luisrmendes <lrmmendes98@gmail.com>
2024-09-24 23:20:28 +0100

    trying to improve deploy script

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -11,12 +11,21 @@ else
     tmux new-session -s $tmuxSessionName -d
 fi
 
-tmux send-keys -t $tmuxSessionName "^C" Enter
-tmux send-keys -t $tmuxSessionName "^C" Enter
-tmux send-keys -t $tmuxSessionName "^C" Enter
-tmux send-keys -t $tmuxSessionName "sleep 1" Enter
-tmux send-keys -t $tmuxSessionName "^C" Enter
-tmux send-keys -t $tmuxSessionName "sleep 1" Enter
+# tmux send-keys -t $tmuxSessionName "^C" Enter
+# tmux send-keys -t $tmuxSessionName "^C" Enter
+# tmux send-keys -t $tmuxSessionName "^C" Enter
+# tmux send-keys -t $tmuxSessionName "sleep 1" Enter
+# tmux send-keys -t $tmuxSessionName "^C" Enter
+# tmux send-keys -t $tmuxSessionName "sleep 1" Enter
+
+# Kill all instances of the process by name
+killall target/release/rush
+
+# Wait for all processes to be terminated
+while pgrep target/release/rush > /dev/null; do sleep 1; done
+
+# Notify when the processes are terminated
+echo "All instances of 'target/release/rush' have been terminated."
 
 tmuxSeshCommand=""
 tmuxSeshCommand="${tmuxSeshCommand} TELOXIDE_TOKEN=$1 ESP8266_ADDRESS_PORT=$2 SYRINX_VARS=$3 SNOWDOG_VARS=$4 THINKPADX1_VARS=$5 RPI5_VARS=$6 RPI3_VARS=$7"
