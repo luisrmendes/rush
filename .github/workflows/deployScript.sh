edf4317e823ef33e8c7be90eaf09529e816b7e06
luisrmendes <lrmmendes98@gmail.com>
2024-09-24 19:04:28 +0100

    trying to fix deploy script

--- .github/workflows/deployScript.sh
+++ .github/workflows/deployScript.sh
@@ -13,6 +13,7 @@ fi
 
 tmux send-keys -t $tmuxSessionName "^C" Enter
 tmux send-keys -t $tmuxSessionName "^C" Enter
+tmux send-keys -t $tmuxSessionName "sleep 2" Enter
 
 tmuxSeshCommand=""
 tmuxSeshCommand="${tmuxSeshCommand} TELOXIDE_TOKEN=$1 ESP8266_ADDRESS_PORT=$2 SYRINX_VARS=$3 SNOWDOG_VARS=$4 THINKPADX1_VARS=$5 RPI5_VARS=$6 RPI3_VARS=$7"
